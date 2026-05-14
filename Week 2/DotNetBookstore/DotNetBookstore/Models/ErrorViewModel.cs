f3aa0302ec79c9ee6f0fd826b3845e4edec78612
Josh Junnola <joshjunnola@gmail.com>
2026-05-14 19:03:53 -0400

    added week 1/2

---
+++ Week 2/DotNetBookstore/DotNetBookstore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
