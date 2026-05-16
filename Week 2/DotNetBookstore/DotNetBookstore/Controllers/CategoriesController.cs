57afc592aceb88d673aa552f52bebbcf64c6c9f6
Josh Junnola <joshjunnola@gmail.com>
2026-05-16 17:16:37 -0400

    Completed week 1

---
+++ Week 2/DotNetBookstore/DotNetBookstore/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DotNetBookstore.Models;
+
+namespace DotNetBookstore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        public IActionResult Greet(string name, int times = 1)
+        {
+            ViewBag.Name = name;
+            ViewBag.Times = times;
+            return View();
+        }
+        
+        public IActionResult List()
+        {
+            ViewData["Message"] = "Welcome to the Categories PAge!";
+            ViewBag.Note = "Data recieved from ViewBag";
+
+            var categories = CategoryDB.GetCategories();
+            return View(categories);
+        }
+
+        public IActionResult Featured()
+        {
+            var featured = new Category
+            {
+                CategoryId = 99,
+                Name = "Featured Category"
+            };
+            return View(featured);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var category = CategoryDB.GetCategory(id);
+            return View(category);
+        }
+    }
+}
---
+++ Week 2/DotNetBookstore/DotNetBookstore/Models/CategoryDB.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetBookstore.Models
+{
+    public static class CategoryDB
+    {
+        private static List<Category> _categories = new List<Category>
+        {
+            new Category { CategoryId = 1, Name = "Fiction" },
+            new Category { CategoryId = 2, Name = "Science" },
+            new Category { CategoryId = 3, Name = "History" },
+            new Category { CategoryId = 4, Name = "Science-Fiction"}
+        };
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategory(int id) =>
+            _categories.FirstOrDefault(c => c.CategoryId == id);
+    }
+}
